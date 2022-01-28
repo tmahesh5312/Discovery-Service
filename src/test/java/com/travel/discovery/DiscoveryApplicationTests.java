ff584b870bedf7d8ac2a6e49df0ca57cd9eb4648
tmahesh5312 <98272517+tmahesh5312@users.noreply.github.com>
2022-01-29 01:27:47 +0530

    Add files via upload

---
+++ src/test/java/com/travel/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travel.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
